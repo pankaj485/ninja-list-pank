494c0a7029284118d2ab6a5f92437e82dee32c63
pankaj485 <pankajsamar2@gmail.com>
2022-03-28 10:16:14 +0530

    add dynamic routes

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import Title from "../../components/Title";
 import styles from "../../styles/Ninjas.module.css";
+import Link from "next/link";
 
 // fetching the content from API and returing
 // this runs before comopnent
@@ -22,11 +23,11 @@ const Ninjas = (props) => {
 				<h1>All ninjas</h1>
 				{ninjas.map((ninja) => {
 					return (
-						<div key={ninja.id}>
+						<Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
 							<a className={styles.single}>
 								<h3>{ninja.name}</h3>
 							</a>
-						</div>
+						</Link>
 					);
 				})}
 			</div>
